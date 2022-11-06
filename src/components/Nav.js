938aeac954cc7a2a1456c7f32165f0754daa8a2a
aleksandr-wegele <109079074+aleksandr-wegele@users.noreply.github.com>
2022-11-06 04:30:49 +0300

    feat: add components Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,21 @@
 import React from "react";
+import AppContext from "../AppContext";
+
+function Nav() {
+    const {setTheme} = React.useContext(AppContext);
+
     const toggleThemeLight = () =>{
         setTheme('light')
       }
       const toggleThemeDark = () => {
         setTheme('dark');
       }
+    return (
+      <div className="Nav">
         <button onClick={toggleThemeLight}>Light</button>
         <button onClick={toggleThemeDark}>Dark</button>
+      </div>
+    );
+  }
+  
+  export default Nav;
